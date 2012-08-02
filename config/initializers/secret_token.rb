2a0839d362e585c045aced5caabd024bda6d72c9
Stephanie Shupe <shupe.stephanie@gmail.com>
2012-08-02 13:58:55 -0700

    add lists table and initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ListMaker::Application.config.secret_token = '2470023bd72be50be109bdba3631420297aba6855234886c764e6ef831d63664b9a6f2164ca792fa3ebe84026ef80114d9419b77c55bceeb7854fd945f942977'
